acd45af4f4682fbaca349023c33e4f214a9b40bb
Meet Shah <meet30012002@gmail.com>
2024-11-27 00:39:15 -0500

    Starter page setup

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+import SignUpScreen from './screens/Auth/SignUpScreen';
+import SignInScreen from './screens/Auth/SignInScreen';
+import EventListScreen from './screens/Events/EventListScreen';
+import FavoritesScreen from './screens/Events/FavoritesScreen';
+
+const AuthStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function AuthNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthStack.Navigator>
+      <AuthStack.Screen name="SignIn" component={SignInScreen} />
+      <AuthStack.Screen name="SignUp" component={SignUpScreen} />
+    </AuthStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AppNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Events" component={EventListScreen} />
+      <Tab.Screen name="Favorites" component={FavoritesScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <NavigationContainer>
+      {user ? <AppNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+}
